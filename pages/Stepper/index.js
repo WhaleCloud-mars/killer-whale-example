8c20c0905142d4e7df5c7e203bbb440616381015
lwq321 <825780118@qq.com>
2018-11-19 10:53:12 +0800

    stepper修改

--- pages/Stepper/index.js
+++ pages/Stepper/index.js
@@ -3,9 +3,6 @@ import { View,Text } from 'react-native';
 import {StepperItem}   from "combination";
 
 
-// 发现一个问题，现在蓝牙选择的设备跟后台没有扯上关系，比如我选择一次活动a,无法知道对应教师设备id是多少，老师可以随便选择蓝牙进行连接，
-// 返回的数据肯定是对应的蓝牙下绑定的学生设备 
-
 export default class StepperItems extends React.Component{
 
     render(){
